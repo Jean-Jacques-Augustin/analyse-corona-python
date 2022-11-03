d5b08703fdfd651bdab5103cd9b05542e643b6be
Jean-Jacques-Augustin <rjja@outlook.fr>
2022-11-03 19:19:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# @TODO: Projet Machine Learning Michel
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+# TODO: Identifier le vrais signe de coronavirus en fonction de resultat des analyse dans une hospital donnée et/ou OMS
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
